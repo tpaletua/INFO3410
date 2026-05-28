4478da55b34ea4f9284b3f93884695353e94e775
tpaletua <10803828@uvu.edu>
2026-05-28 15:21:21 -0600

    sql query changes

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Publishers;
+
+
+--Populate the Publishers table
+INSERT INTO Publishers
+VALUES ('Tor Science Fiction'),
+('Disney-Hyperion'),
+('Wiley'),
+('O''Reilly'),
+('Murach & Associates'),
+('Scholastic'),
+('Infinte Publishing');
+GO
